d2b356daad39f39595d0c500ad431d9a3a38d3af
GreekFellows <unknownuser826@gmail.com>
2015-02-01 16:30:34 +0800

    a less edit
    a less edit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require("express")();
 var http = require("http").Server(app);
 
 app.get("/", function(req, res) {
-	res.sendFile(__dirname + "/index.html");
+	res.sendFile(__dirname + "/hidden.html");
 });
 
 http.listen(3000, function() {
